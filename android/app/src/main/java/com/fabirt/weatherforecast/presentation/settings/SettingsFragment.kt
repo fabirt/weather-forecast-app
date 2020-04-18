e1f3945748968896e96398b64fe627fa68c5c16a
Fabirt <fandig27@gmail.com>
2020-04-18 15:37:26 -0500

    Adding functions to launch intents in settings

--- android/app/src/main/java/com/fabirt/weatherforecast/presentation/settings/SettingsFragment.kt
+++ android/app/src/main/java/com/fabirt/weatherforecast/presentation/settings/SettingsFragment.kt
@@ -1,33 +1,58 @@
 package com.fabirt.weatherforecast.presentation.settings
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.fabirt.weatherforecast.databinding.FragmentSettingsBinding
 
-import com.fabirt.weatherforecast.R
 
 class SettingsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = SettingsFragment()
-    }
-
+    private lateinit var binding: FragmentSettingsBinding
     private lateinit var viewModel: SettingsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+        binding.locationPermissionSwitch.setOnCheckedChangeListener { _, isChecked ->
+            locationPermissionsCheckedChangeListener(isChecked)
+        }
+
+        binding.locationEnabledSwitch.setOnCheckedChangeListener { _, isChecked ->
+            locationEnabledCheckedChangeListener(isChecked)
+        }
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
+    private fun locationPermissionsCheckedChangeListener(isChecked: Boolean) {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            .addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+            .addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+        val uri: Uri = Uri.fromParts("package", context?.packageName, null)
+        intent.data = uri
+        startActivity(intent)
+    }
+
+    private fun locationEnabledCheckedChangeListener(isChecked: Boolean) {
+        val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            .addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+            .addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+        startActivity(intent)
+    }
 }
